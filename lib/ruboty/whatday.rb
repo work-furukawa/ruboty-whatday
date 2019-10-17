1b2f252ee426159282b814feaef9a2483914cf30
work.furukawa <work.furukawa@gmail.com>
2019-10-17 23:36:36 +0900

    first commit

---
+++ lib/ruboty/whatday.rb
@@ -0,0 +1,8 @@
+require "ruboty/whatday/version"
+
+module Ruboty
+  module Whatday
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/whatday/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Whatday
+    VERSION = "0.1.0"
+  end
+end
